c4e6807d6c34e8bcd1d05726d54016625ee0d227
ldvndev <ldvndev@gmail.com>
2023-12-02 16:10:43 -0300

    feat: add styles global

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
   return(
-    <strong>Hello world!</strong>
-  )
+    <ThemeProvider theme={defaultTheme}>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
