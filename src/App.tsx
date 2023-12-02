6b44fd635a345e50a519f03aca0b868739462ed2
ldvndev <ldvndev@gmail.com>
2023-12-02 16:45:51 -0300

    feat: add ESLint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { ThemeProvider } from 'styled-components';
-
-import { GlobalStyle } from './styles/global';
-import { defaultTheme } from './styles/themes/default';
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  return(
+  return (
     <ThemeProvider theme={defaultTheme}>
-
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
