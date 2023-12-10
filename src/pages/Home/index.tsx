a106c8da6567e7aef92e2e264f00e84687bc8674
ldvndev <ldvndev@gmail.com>
2023-12-09 22:01:27 -0300

    feat: add interface NewCycleFormData

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,12 +21,21 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser no máximo de 60 minutos'),
 })
 
+interface NewCycleFormData {
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
