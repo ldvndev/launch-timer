63d03b1d2a0e4e44fc16c70e85fa6a281cbc14d1
ldvndev <ldvndev@gmail.com>
2023-12-22 14:03:36 -0300

    feat: Add date formatting

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -66,8 +66,6 @@ export function CycleContextProvider({ children }: CycleContextProviderProps) {
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
